d3120921cbf3a797b48524a8439b9694528b7c41
Jamie Terry <jnterry@ntlworld.com>
2018-02-19 21:03:46 +0000

    Add jsdoc style documentation

--- nano-orm.js
+++ nano-orm.js
@@ -25,6 +25,33 @@ let Q = require('q');
 /// \param options JSON object representing additional options for the model
 ///        - id_field -> Name of the id field, defaults to 'id'
 //////////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * Creates a new class for representing a Model which may be loaded from
+ * and persisted to the database
+ *
+ * @param {string} table_name - The name of the table in the database the created Model
+ * should wrap
+ *
+ * @param {Object[]} model_fields - Array of field descriptors. Order must be
+ * that of the columns in the database. Each field descriptor may be either a
+ * string holding the field's name, or a JSON schema for the type stored by the
+ * field. Important properties of the JSON schema are outlined below.
+ * @param {string} model_fields[].name - Name of the field. Must be equal to the
+ * name of the column in the database
+ * @param {bool} [model_fields[].required] - Whether the field is required
+ * (IE: cannot be NULL). Defaults to true.
+ *
+ * @param {Object} [options] - JSON object representing the additional options for
+ * the model.
+ * @param {string} [options.id] - Name of the id field, defaults to 'id'
+ *
+ * @todo :TODO: Is there anything other than id field name to put in options?
+ * Should we just have it as a string
+ *
+ * @return Constructor for the created Model class
+ */
 function defineModel(table_name, model_fields, options){
 	//console.info("Creating Model for table: '" + table_name + "'");
 
@@ -48,13 +75,21 @@ function defineModel(table_name, model_fields, options){
 	let id_field_name = "id";
 	if(options && options.id_field){ id_field_name = options.id_field; }
 
-	/////////////////////////////////////////////////////////////////////
-	/// \brief Constructor which creates new instance of some model
-	/// \param field_values Object representing the values to use for the
-	/// fields of the model. Any fields that are not specified will be assigned
-	/// null. If extra values are put in field_values for which a field does
-	/// not actually exist, then this method will throw.
-	/////////////////////////////////////////////////////////////////////
+	/**
+	 * Creates a new instance of the Model with specified values for fields.
+	 * This function will NEVER affect the database itself.
+	 *
+	 * @constructor
+	 *
+	 * @param {Object} [field_values] JSON object with values to use for the
+	 * fields of the created model instance. Any field's whose value is not
+	 * specified will be set to null. Attempting to set a field which does not
+	 * exist will result in an exception.
+	 *
+	 * @todo Throw error if attempt to specify id field -> we don't want arbitrary
+	 * ids or it will look as though it exists in the db, may override existing
+	 * rows, etc
+	 */
 	let Model = function(field_values){
 		// Define fields of the model
 		this._fields = {};
@@ -106,17 +141,54 @@ function defineModel(table_name, model_fields, options){
 	};
 
 	Model._queries = {}; // cache of sql statements
+
+	/**
+	 * Retrieves the name of the table this Model wraps
+	 * @return {string} Name of wrapped table
+	 */
 	Model.getTableName   = (function(){ return table_name;                      });
+
+	/**
+	 * @note This does not include the name of the id field
+	 * @see getIdFieldName
+	 *
+	 * @return {string[]} Array of field names in this model
+	 */
 	Model.getFieldNames  = (function(){ return model_fields.map((f) => f.name); });
+
+	/**
+	 * Retrieves the name of the id field for this model
+	 *
+	 * @note instance.id can always be used to access the value of the id - this
+	 * function is useful only if writing plain SQL queries that require the id
+	 * field name
+	 *
+	 * @return {string} Name of id field
+	 */
 	Model.getIdFieldName = (function(){ return id_field_name;                   });
 
+	/**
+	 * Converts a Model to an object that which should be converted to JSON
+	 * to represent the Model's data. This will include the values of the
+	 * Model's fields only, with no associated meta data (eg: the name of
+	 * table which stores Model
+	 *
+	 * @return {object}
+	 */
 	Model.prototype.toJSON = function(){ return this._fields; };
 
-	/////////////////////////////////////////////////////////////////////
-	/// \brief Creates a new instance of Model and persists it to the database
-	/// \return Promise which resolves to the model instance after it has
-	/// been saved
-	/////////////////////////////////////////////////////////////////////
+	/**
+	 * Creates a new instance of the Model and persists it to the database
+	 *
+	 * @param dbh {DbConnectionPromise} -  database handle which which to save
+	 * the Model to the database
+	 * @param field_values {object} - Object containing values for the fields
+	 * of the created instance
+	 *
+	 * return {Promise} - Promise which will either resolve to the created
+	 * instance after it has been saved (and thus assigned an id if the id
+	 * column is AUTO_INCREMENT) or be rejected if an error occurs while saving
+	 */
 	Model.create = function(dbh, field_values){
 		let result = new Model(field_values);
 		return result.save(dbh);
@@ -126,7 +198,18 @@ function defineModel(table_name, model_fields, options){
 	/// \brief Creates an instance of this model from a row loaded from the
 	/// database are returned by db-connection-promise.query
 	/////////////////////////////////////////////////////////////////////
+	/**
+	 * Creates a new instance of Model from a row of data retrieved from the
+	 * database
+	 *
+	 * @note In general users of nano-orm should prefer methods such as find()
+	 * or load(), however this may be used to create an instance if you need to
+	 * use custom SQL queries to load a Model instance
+	 *
+	 * @param row {object} - Row of data returned from the database
+	 */
 	Model.createFromRow = function(row){
+		// :TODO:COMP: can we just use constructor?
 		let model = new Model();
 
 		// Check id is availible and set
@@ -151,11 +234,12 @@ function defineModel(table_name, model_fields, options){
 		return model;
 	};
 
-	/////////////////////////////////////////////////////////////////////
-	/// \brief Creates an instance of the model from a list of rows
-	/// as returned by database .query
-	/// \return Promise which will evaluate to an array of instances of this Model
-	/////////////////////////////////////////////////////////////////////
+	/**
+	 * Creates an array of Model instances from an array of rows returned from the
+	 * database
+	 *
+	 * @param rows {Object[]} - Array of rows returned from the database
+	 */
 	Model.createFromRows = function(rows) {
 		if(rows.rowCount != null && rows.rows != null && rows.fields != null){
 			// This this is the result directly from the database
@@ -169,9 +253,34 @@ function defineModel(table_name, model_fields, options){
 		return result;
 	};
 
-	Model.prototype.isDirty     = function() { return this._dirty; };
+	/**
+	 * Determines if the Model instance is different to the corresponding entry
+	 * in the database, or has not yet been persisted to the database
+	 *
+	 * An instance will automatically be marked as dirty whenever any field
+	 * value is modified. Additionally a new instance will be dirty as it will
+	 * not yet have been persisted to the database
+	 *
+	 * @return {boolean}
+	 */
+	Model.prototype.isDirty = function() { return this._dirty; };
+
+	/**
+	 * Marks the model instance as dirty - IE: differs from the corresponding
+	 * entry in the database.
+	 *
+	 * @note It is uncommon to need to call this method - modifying any field
+	 * values will automatically set the Model instance as dirty
+	 */
 	Model.prototype.markAsDirty = function() { this._dirty = true; };
-	Model.prototype.getModel    = function() { return Model;       };
+
+	/**
+	 * Retrieves the Model class an Model instance is created from. Typically
+	 * only required to call static methods of the Model if given an instance
+	 *
+	 * @return {Model}
+	 */
+	Model.prototype.getModel = function() { return Model; };
 
 	_attachQueryFunctions(Model);
 	_attachJsonSchema    (Model, model_fields);
